8c916ab6cfa319e714957d7745a91b0dc3a30306
chensai <chensai@chensaideMacBook-Pro-2.local>
2020-11-03 15:11:11 +0800

    feat: demo完成

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+// 引入recat-router-dom
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+
+// 测试css隔离
 import './App.css';
 
+
 function App() {
+  // 测试js隔离---子应用 设置值
+  window.baseApp = 22;
+
+  const Page2 = () => '我是page2';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      learn react
+      <hr />
+      <BrowserRouter basename="/react">
+        <Link to="/page1" style={{ marginRight: 20 }}>page1</Link>
+        <Link to="/page2">page2</Link>
+        <br />
+        <br />
+        <Route path="/page1" render={() => '我是page1'} />
+        <Route path="/page2" component={Page2} />
+      </BrowserRouter>
     </div>
   );
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// 引入single-spa-react
+// 注意 Singlespacontext 是一个为react@16.3(如果可用的话)提供的上下文，包含了 singleSpa props
+import singleSpaReact, { SingleSpaContext } from 'single-spa-react';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+// 开始对自应用配置
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+  domElementGetter: () => document.querySelector('#reactBox'),
+  errorBoundary(err, info, props) {
+    console.log('err, info, props', err, info, props);
+    return (
+      <div>This renders when a catastrophic error occurs</div>
+    );
+  },
+})
+
+// 处理公共路径和实例
+if (!window.singleSpaNavigate) {
+  // 不是作为子应用时，可以独立启动
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+export const bootstrap = reactLifecycles.bootstrap;
+export const mount = reactLifecycles.mount;
+export const unmount = reactLifecycles.unmount;
